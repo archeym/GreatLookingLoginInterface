0e72aa5a4c7f129f61dca9b86361e5d7763f0f02
your name <your@email.com>
2017-06-26 10:47:08 +0100

    Creating the Modal Popup

---
+++ GreatLookingLoginInterface/SignUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SignUpViewController.swift
+//  GreatLookingLoginInterface
+//
+//  Created by Arkadijs Makarenko on 26/06/2017.
+//  Copyright © 2017 ArchieApps. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func singUp(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func dismissPopUp(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
